6091719e786be3910c4186308b082368c4c27d32
unknown <raderk0777@gmail.com>
2020-07-31 18:46:58 -0700

    added a drink system! i did a thing

--- script.js
+++ script.js
@@ -3,6 +3,19 @@
 
 // }
 $(document).ready(function(){
+    var drinkList = [{name: "coek", price: "$2"}, {name: "peepsi", price: "$2"}, {name: "1up", price: "$3"}, {name: "siript", price: "$2"}]
+    $("#drinkthing").click(function(){
+        for(i=0;i<drinkList.length;i++){
+            var dronk = $("<div>")
+            dronk.addClass("drink")
+            dronk.text(drinkList[i].name + " is " + drinkList[i].price + " pls pay us we is pore")
+            $("#dronk").append(dronk)
+        }
+
+
+        
+
+    })
     $("button").click(function(){
         $("#div1").fadeIn();
         $("#div2").fadeIn("slow");
@@ -13,12 +26,12 @@ $(document).ready(function(){
     });   
     $("#something").click(function(){
         var newDiv = $("<p></p>")
-        newDiv.text('am tired today')
+        newDiv.text('am no longer tired today')
         newDiv.addClass("demo2")
         $('#demo1').append(newDiv)
     })
     $("#idk").click(function(){
-        var movie = 'speak'
+        var movie = 'a silent voice'
         var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
         $.ajax({
             url: queryURL,
@@ -26,7 +39,7 @@ $(document).ready(function(){
         }).then(function(response){
             console.log(response)
         })
-
+    
 
     })
 })
